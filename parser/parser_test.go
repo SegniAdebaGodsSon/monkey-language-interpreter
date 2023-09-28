9eeeb5f7a6eed4b50dff33982762e0bb4537655c
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2023-09-28 19:04:52 +0300

    parsing grouped expressions working

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -354,7 +354,7 @@ func TestOperatorPrecendenceParsing(t *testing.T) {
 			"((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
 		},
 		{
-			"1 + (2 + 3) + 4)",
+			"1 + (2 + 3) + 4",
 			"((1 + (2 + 3)) + 4)",
 		},
 		{
