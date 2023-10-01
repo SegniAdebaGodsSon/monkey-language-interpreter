175062e23a6db7a289af8a3a5b6c1f84ce102136
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2023-10-01 19:49:24 +0300

    evaluator for intege and boolean expressions added, also for prefix expressions

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey/evaluator"
 	"monkey/lexer"
 	"monkey/parser"
 )
@@ -16,7 +17,6 @@ func Start(in io.Reader, out io.Writer) {
 	for {
 		fmt.Printf(PROMT)
 		scanned := scanner.Scan()
-
 		if !scanned {
 			return
 		}
@@ -31,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
